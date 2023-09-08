c4cbf3b424136ac1a70806a1c591092b04eed294
hfutrell-gss <hfutrell@gssmail.com>
2023-09-08 14:21:56 -0500

    Test helper will throw when null

--- Shared.Kernel.XUnitHelpers/ResultTestExtensions.cs
+++ Shared.Kernel.XUnitHelpers/ResultTestExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using Shared.Abstractions.Results;
 using Xunit;
+using Xunit.Sdk;
 
 namespace Shared.Kernel.TestHelpers;
 
@@ -8,28 +9,28 @@ public static class ResultTestExtensions
     public static void AssertSuccessful<TS>(this ResultBase<TS> result)
     {
         result.Resolve(
-            _ => Assert.True(true),
-            _ => Assert.False(true, "Expected successful result"));
+            _ => Assert.NotNull(result.SuccessValue),
+            _ => throw new XunitException("Result failed when success was expected"));
     }
 
     public static void AssertFailure<TS>(this ResultBase<TS> result)
     {
         result.Resolve(
-            _ => Assert.False(true, "Expected failed result"),
-            _ => Assert.True(true));
+            _ => throw new XunitException("Result succeeded when failure was expected"),
+            _ => Assert.NotNull(result.FailureDetails));
     }
 
     public static FailureDetails ExpectFailureAndGet<TS>(this ResultBase<TS> result)
     {
         return result.Resolve(
-            _ => throw new Exception("Expected failure"),
+            _ => throw new XunitException("Expected failure"),
             fail => fail);
     }
 
     public static TS ExpectSuccessAndGet<TS>(this ResultBase<TS> result)
     {
         return result.Resolve(
-            success => success,
-            _ => throw new Exception("Expected success"));
+            success => success is not null ? success : throw new XunitException("Result was null"),
+            _ => throw new XunitException("Expected success"));
     }
 }
